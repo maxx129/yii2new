a119dc1148bc3ce65b90477c7f3fa2ae6455e81e
vagrant <vagrant@yii2new>
2019-08-10 08:24:14 +0100

    100819 1023

---
+++ frontend/controllers/ForTestGitController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace frontend\controllers;
+
+/**
+ * Description of ForTestGitController
+ *
+ * @author Max
+ */
+class ForTestGitController {
+    //put your code here
+}
