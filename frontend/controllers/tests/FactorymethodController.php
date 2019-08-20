f8dc4052f64a78f9b221ef37a3731913e42aefa1
maxx129 <maxx129@yandex.ru>
2019-08-20 07:39:48 +0300

    200819 0739

---
+++ frontend/controllers/tests/FactorymethodController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\controllers\tests;
+
+use yii\web\Controller;
+
+class FactorymethodController extends Controller {
+    
+    public function actionIndex() {
+            echo "OK!!!!!!";die;
+    }
+
+}
