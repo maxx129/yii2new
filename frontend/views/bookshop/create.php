8a0150f7e1711e2728a4e1dc12f87a38ff9158d4
maxx129 <maxx129@yandex.ru>
2019-08-27 20:05:54 +0300

    270819 2005

--- frontend/views/bookshop/create.php
+++ frontend/views/bookshop/create.php
@@ -7,10 +7,10 @@ use yii\helpers\Html;
 
 <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($book, 'name'); ?>
-    <?= $form->field($book, 'isbn'); ?>
-    <?= $form->field($book, 'date_published'); ?>
-    <?= $form->field($book, 'publisher_id'); ?>
+    <?= $form->field($book, 'name')->textInput(['style' => 'width:450px']); ?>
+    <?= $form->field($book, 'isbn')->textInput(['style' => 'width:450px']); ?>
+    <?= $form->field($book, 'date_published')->textInput(['style' => 'width:450px']); ?>
+    <?= $form->field($book, 'publisher_id')->textInput(['style' => 'width:450px']); ?>
 
     <?= Html::submitButton('Save', [
         'class' => 'btn btn-prymary',
